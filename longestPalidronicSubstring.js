a4ddb1232c1324441181c9aa9cbfcf598f12fc55
michellecarey <genovese28@yahoo.com>
2018-10-22 09:37:29 -0700

    adjustments

---
+++ longestPalidronicSubstring.js
@@ -0,0 +1,11 @@
+// Given a string find the longest substring substring
+
+
+// outputs: string
+// inputs: string
+// constraints:?
+// edge cases: aren't any, all same letter
+
+//console.log(longestPalidrone("babad"))// aba
+//console.log(longestPalidrone("cbbd"))//bb
+
