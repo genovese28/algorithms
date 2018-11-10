8fbccaad0c70fa26bdce516ee8ec326b3038f5c1
michellecarey <genovese28@yahoo.com>
2018-11-10 14:14:00 -0800

    adding removeElements

---
+++ snail.js
@@ -0,0 +1,12 @@
+// Write a function called snail which Given an n x n array,
+// returns the array elements arranged from outermost
+// elements to the middle element, traveling clockwise.
+
+//snail([[1,2,3],[4,5,6],[7,8,9]] ) // [1,2,3,6,9,8,7,4,5]
+
+[[1, 2, 3], [1, 2, 3], [4, 5, 6], [8, 9, 4], [7, 8, 9]][(7, 6, 5)];
+
+//snail([[1,2,3],[8,9,4],  [7,6,5]]) // [1,2,3,4,5,6,7,8,9]
+
+//snail([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]])
+// [1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13]
