6964935c8a9ddfb1bcc7647470d39b05b8756011
michellecarey <genovese28@yahoo.com>
2018-11-12 21:43:43 -0800

    done

--- removeElement.js
+++ removeElement.js
@@ -9,8 +9,11 @@
 var output = removeElement([1, 2, 3, 2, 1], 2);
 console.log(output); // --> [1, 3, 1]
 
-
 function removeElement(array, discarder) {
-  // your code here
-
-
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === discarder) {
+      array.splice(i, 1);
+    }
+  }
+  return array;
+}
